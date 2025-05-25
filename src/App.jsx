c6306e57f9e82e3f2d7dd55620d0ef67dba95ba3
jongin <0422ll@snu.ac.kr>
2025-05-25 22:49:31 +0900

    add Header Prompt Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import "./App.css";
+import MainPage from "./routes/MainPage";
+import { Header, Prompt } from "@/components";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+
+function AppContent() {
+  const location = useLocation();
+
+  const HIDE_HEADER_PATHS = [];
+  const HIDE_PROMPT_PATHS = [];
+
+  const shouldShowHeader = !HIDE_HEADER_PATHS.includes(location.pathname);
+  const shouldShowPrompt = !HIDE_PROMPT_PATHS.includes(location.pathname);
+
+  return (
+    <>
+      {shouldShowHeader && <Header />}
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+      {shouldShowPrompt && <Prompt />}
+    </>
+  );
+}
 
 function App() {
   return (
-    <>
-      <div></div>
-      <h1>chatGPT</h1>
-    </>
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
   );
 }
 
